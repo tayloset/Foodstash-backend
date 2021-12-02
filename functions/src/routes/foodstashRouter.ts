d0605806b8d4094339eb3946ff84c32b18a99256
seth <sethjtaylor05@gmail.com>
2021-12-02 11:29:35 -0500

    routes/backend

---
+++ functions/src/routes/foodstashRouter.ts
@@ -0,0 +1,18 @@
+// import { ObjectId } from "bson";
+// import express from "express";
+// import { getClient } from "../db";
+
+// const foodstashRouter = express.Router();
+// const errorResponse = (error: any, res: any) => {
+//     console.error("FAIL", error);
+//     res.status(500).json({ message: "Internal Server Error" });
+//   };
+
+//   foodstashRouter.post("/:userId", async (req,res) =>{
+//  const newUser: User =
+//       try {
+
+//       } catch (error) {
+//         errorResponse(error, res);
+//       }
+//   })
