212909b1262b328841dcfe84881016646232fdcb
seth <sethjtaylor05@gmail.com>
2021-12-07 11:24:55 -0500

    put method

--- functions/src/routes/foodstashRouter.ts
+++ functions/src/routes/foodstashRouter.ts
@@ -67,36 +67,56 @@ foodstashRouter.post("/:id", async (req, res) => {
   }
 });
 
-foodstashRouter.put("/:id/:category", async (req, res) => {
+// foodstashRouter.put("/:id/:category", async (req, res) => {
+//   try {
+//     const id: string = req.params.id;
+//     const category: string = req.params.category;
+//     const categoryData = req.body.updateData;
+//     const query: any = {};
+//     query[category] = categoryData;
+//     console.log(id, category, req.body, categoryData, query, query[category]);
+//     const client = await getClient();
+//     if (typeof categoryData === "string") {
+//       await client
+//         .db()
+//         .collection<Profile>("profiles")
+//         .updateOne({ uid: id }, { $push: query });
+//     } else {
+//       await client
+//         .db()
+//         .collection<Profile>("profiles")
+//         .updateOne({ uid: id }, { $set: query });
+//     }
+//     const client1 = await getClient();
+//     const results = await client1
+//       .db()
+//       .collection<Profile>("profiles")
+//       .find({ uid: id })
+//       .toArray();
+//     res.status(201).json(results);
+//     console.log(results);
+//   } catch (err) {
+//     errorResponse(err, res);
+//   }
+// });
+
+foodstashRouter.put("/:uid", async (req, res) => {
   try {
-    const id: string = req.params.id;
-    const category: string = req.params.category;
-    const categoryData = req.body.updateData;
-    const query: any = {};
-    query[category] = categoryData;
-    console.log(id, category, req.body, categoryData, query, query[category]);
+    const uid: string = req.params.uid;
+    const updatedProfile: Profile = req.body;
+    console.log(updatedProfile);
     const client = await getClient();
-    if (typeof categoryData === "string") {
-      await client
-        .db()
-        .collection<Profile>("profiles")
-        .updateOne({ uid: id }, { $push: query });
-    } else {
-      await client
-        .db()
-        .collection<Profile>("profiles")
-        .updateOne({ uid: id }, { $set: query });
-    }
-    const client1 = await getClient();
-    const results = await client1
+    const result = await client
       .db()
       .collection<Profile>("profiles")
-      .find({ uid: id })
-      .toArray();
-    res.status(201).json(results);
-    console.log(results);
-  } catch (err) {
-    errorResponse(err, res);
+      .replaceOne({ uid }, updatedProfile);
+    if (result.modifiedCount) {
+      res.status(200).json(updatedProfile);
+    } else {
+      res.status(404).send("Not Found");
+    }
+  } catch (error) {
+    errorResponse(error, res);
   }
 });
 
