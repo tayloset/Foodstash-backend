b953c01401cdead25a75b39cc35e9cffe84a1202
sethkasten <kastens@umich.edu>
2021-12-08 10:33:53 -0500

    unknown

--- functions/src/routes/foodstashRouter.ts
+++ functions/src/routes/foodstashRouter.ts
@@ -54,39 +54,6 @@ foodstashRouter.post("/:id", async (req, res) => {
   }
 });
 
-// foodstashRouter.put("/:id/:category", async (req, res) => {
-//   try {
-//     const id: string = req.params.id;
-//     const category: string = req.params.category;
-//     const categoryData = req.body.updateData;
-//     const query: any = {};
-//     query[category] = categoryData;
-//     console.log(id, category, req.body, categoryData, query, query[category]);
-//     const client = await getClient();
-//     if (typeof categoryData === "string") {
-//       await client
-//         .db()
-//         .collection<Profile>("profiles")
-//         .updateOne({ uid: id }, { $push: query });
-//     } else {
-//       await client
-//         .db()
-//         .collection<Profile>("profiles")
-//         .updateOne({ uid: id }, { $set: query });
-//     }
-//     const client1 = await getClient();
-//     const results = await client1
-//       .db()
-//       .collection<Profile>("profiles")
-//       .find({ uid: id })
-//       .toArray();
-//     res.status(201).json(results);
-//     console.log(results);
-//   } catch (err) {
-//     errorResponse(err, res);
-//   }
-// });
-
 foodstashRouter.put("/:uid", async (req, res) => {
   try {
     const uid: string = req.params.uid;
